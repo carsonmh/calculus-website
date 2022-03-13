77a28ba87739fc38bb9a318fd7038ce84a705290
carsonharrell2 <thecarsonharrell@gmail.com>
2022-03-13 13:55:44 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var userbase = [
+    {
+        username: "carson",
+        password: "secret"
+    }
+];
+
+var userNamePrompt = prompt("whats your username");
+var passwordPrompt = prompt("whats your password");
+
+function signIn(username, password) {
+    
+}
