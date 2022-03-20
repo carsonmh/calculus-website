5b2e86f1586cdd230767293c4256e80a20ad5ab6
carsonharrell2 <thecarsonharrell@gmail.com>
2022-03-19 17:28:38 -0700

    log in and register html and css

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-var userbase = [
-    {
-        username: "carson",
-        password: "secret"
-    }
-];
+// var userbase = [
+//     {
+//         username: "carson",
+//         password: "secret"
+//     }
+// ];
 
-var userNamePrompt = prompt("whats your username");
-var passwordPrompt = prompt("whats your password");
+// var userNamePrompt = prompt("whats your username");
+// var passwordPrompt = prompt("whats your password");
 
-function signIn(username, password) {
-    
-}
+// function signIn(username, password) {
+//     if (username === database[0].username && password === database[0].password) {
+//         console.log("working")
+//     }else {
+//         console.log("failed")
+//     }
+// }
+
