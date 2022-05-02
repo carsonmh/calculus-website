b8f2c25c4bcdc9d9505ae1b596259e478c62c51d
carsonharrell2 <thecarsonharrell@gmail.com>
2022-05-01 21:40:54 -0700

    animation play button and functionality

--- visualizations/squeeze-theorem.js
+++ visualizations/squeeze-theorem.js
@@ -1,8 +1,40 @@
-function setup() {
-    var cnv = createCanvas(400, 400);
+var x = 20;
+
+function setup (){
+  size = 400;
+  cnv = createCanvas(size, size);
+  buttonShow = true;
+  playAnimation = false;
+}
+
+function draw (){
+  background(230);
+  fill(51, 102, 138);
+  strokeWeight(1.5);
+  stroke(150);
+  width=100;
+  height=55;
+  buttonPosx = size/2 - width/2;
+  buttonPosy = size/2 - height/2;
+  if(buttonShow === true){
+      rect(buttonPosx, buttonPosy, width, height, 15);
+    fill(150);
+      triangle(buttonPosx + 0.43 * width, buttonPosy + 0.38 * height, buttonPosx + 0.43 * width, buttonPosy + 0.62 * height, buttonPosx + 0.57 * width, buttonPosy + 0.5 * height);
   }
-  
-  function draw() {
-    background(220);
-    ellipse(50,50,80,80);
+  if(playAnimation === true) {
+    play();
   }
+}
+
+function play (){
+  // insert code for animation here //
+  ellipse(x, 50, 80, 80);
+  x += 1;
+}
+
+function mouseClicked() {
+  if((buttonPosx < mouseX && mouseX < (buttonPosx + width)) && (buttonPosy < mouseY && mouseY < (buttonPosy + height))) {
+    buttonShow = false;
+    playAnimation = true;
+  }
+}
