f88dc940131c2e23e185544cbd00cb4246f24128
Tobias Tornqvist <ttornqvi@ssnc.com>
2022-05-13 23:58:19 -0700

    Added first integral visualization

---
+++ visualizations/riemman1.js
@@ -0,0 +1,105 @@
+//env vars
+const bgcolor = BGCOLOR;
+
+const w = 800, h1 = 600;
+const f = (x) => -pow(x-4, 2) + 16;
+const maxRect = 200;
+
+let back;
+let label = true;
+let anim = true;
+let time = 0;
+let paused = true;
+
+
+function setup() {
+    
+    c = getColors();
+    
+    createCanvas(w, h1);
+    setWindow(-2, 10, -2, 18);
+    background(bgcolor);
+    
+    // axes
+    stroke(255, 100);
+    drawAxes();
+    
+    // grid lines
+    stroke(255, 50);
+    drawGridlines();
+    
+    // function graph
+    strokeWeight(2);
+    stroke(c.g);
+    graph(f);
+    
+    back = get(0, 0, width, height);
+    playButton();
+    noLoop();
+}
+
+
+
+let h = 3;
+function draw() {
+    image(back, 0, 0);
+    const n = anim ? floor(time/30) + 2 : constrain(map(mouseX, 0, width, 2, maxRect), 2, 200);
+    const a = 0, b = 8, deltaX = (b-a) / n;
+    
+    // Riemmen Sum
+    const clr = c.b;
+    let area = 0;
+    
+    fill(red(clr), green(clr), blue(clr), 50);
+    
+    for(let i = a; i < b-0.001; i += deltaX) {
+        const tcorner = worldToScreen(i, f(i));  // top left corner of rect
+        const bcorner = worldToScreen(i, 0);     // bottom left corner of rect
+        const w = deltaX/xstep;
+        noStroke();
+        rect(tcorner.i, tcorner.j, w, bcorner.j-tcorner.j);
+        stroke(clr);
+        line(tcorner.i, tcorner.j, tcorner.i, bcorner.j-2);
+        line(tcorner.i, tcorner.j, tcorner.i+w, tcorner.j);
+        line(tcorner.i+w, tcorner.j, tcorner.i+w, bcorner.j-2);
+        
+        area += deltaX * f(i);
+    }
+    
+    // animation
+    time ++;
+    time %= maxRect*30;
+    
+    // labels
+    if(!label) return;
+    textSize(20);
+    noStroke();
+    fill(255);
+    text("n: " + floor(n), width*0.25, 35);
+    text("Area: " + nround(area, 3), width*0.75, 35);
+    
+    if(paused) playButton();
+}
+
+
+
+// user input
+
+function mouseClicked() {
+    if(paused) {
+        paused = false;
+        loop();
+    } else {
+        time = mouseX;
+        anim = !anim;
+    }
+}
+
+function keyReleased() {
+    if(keyCode == 32) label = !label;
+    if(keyCode == 27) {
+        setup();
+        paused = true;
+    }
+}
+
